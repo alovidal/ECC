c59f613b2cba7cd977d6673e0028d613acc9ae5b
alovidal <135652044+alovidal@users.noreply.github.com>
2024-04-11 21:43:30 -0400

    JS modulo

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
-alert('Hola Mundo')
+const btnCart = document.querySelector('.container-icon')
 
+const containerCartProducts = document.querySelector('.container-cart-product')
+
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart')
+})
